a90908c4ab0bf7bc9f112f4a8f12594b5437f89a
toson <theawerqwer@gmail.com>
2019-11-20 23:40:26 +0900

    オブジェクト動的生成、放射状に移動

--- Assets/scripts/PrefabTest.cs
+++ Assets/scripts/PrefabTest.cs
@@ -8,65 +8,59 @@ public class PrefabTest : MonoBehaviour
 
     public GameObject[] objects;
     private GameObject useobjects;
+    private Quaternion[] forwardAxis;
 
     // Use this for initialization
     void Start()
     {
-        // CubeプレハブをGameObject型で取得
-        // GameObject obj = (GameObject)Resources.Load("MovingCreate");
-        objects = new GameObject[24];
+       
+        objects = new GameObject[100];
+       
        // useobjects = new GameObject[24];
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 100; i++)
         {
-            objects[i]= (GameObject)Resources.Load("MovingCreate");
-            objects[i] = Instantiate(objects[i], new Vector3(0.0f, 2.0f * 0.3f * i, 0.0f), Quaternion.identity);
-            
+            useobjects= (GameObject)Resources.Load("MovingCreate");
+            objects[i] = Instantiate(useobjects, new Vector3(0.0f, 0, 0.0f), Quaternion.identity);
+            objects[i].transform.Rotate(new Vector3(0, 0, i*10));
+
         }
-        // Cubeプレハブを元に、インスタンスを生成、
-        //Instantiate(obj, new Vector3(5.0f, 0.0f, 0.0f), Quaternion.identity); 
+       
         time = 0.0f;
     }
 
     void Update()
     {
-        // CubeプレハブをGameObject型で取得
-        //GameObject obj = (GameObject)Resources.Load("MovingCreate");
-        //StartCoroutine("CreateObj");
-
-        // time += Time.deltaTime;
-        // if( time < 30)
-        // {
-
-        // }
+        time += Time.deltaTime;
 
        
-        for (int i=0; i < 3; i++)
+        for (int i=0; i < 100; i++)
         {
-            objects[i].gameObject.transform.Translate(0, 0.5f, 0);
+            // objects[i].gameObject.transform.Translate(1.0f, 0.5f, 0);
+           //objects[i].transform.Rotate(new Vector3(i*10, 30, 1));
+
+            objects[i].gameObject.transform.Translate(objects[i].transform.right);
         }
 
-        //if (transform.position.y > 5)
-        //{
-        //    Destroy(gameObject);
-        //}
     }
 
-    //IEnumerator CreateObj()
-    //{
-    //    while(true)
-    //    {
-    //        if (true)
-    //        {
-    //            GameObject obj = (GameObject)Resources.Load("MovingCreate");
-    //            Instantiate(obj, new Vector3(0.0f, time, 0.0f), Quaternion.identity);
+    IEnumerator CreateObj()
+    {
+        while (true)
+        {
+            if (true)
+            {
+                GameObject obj = (GameObject)Resources.Load("MovingCreate");
+                Instantiate(obj, new Vector3(0.0f, time, 0.0f), Quaternion.identity);
 
-    //            yield return new WaitForSeconds(0.5f);
-    //        }
-    //        else
-    //        {
-    //            break;
-    //        }
-    //    }
-    //}
+
+
+                yield return new WaitForSeconds(0.5f);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
 
 }
