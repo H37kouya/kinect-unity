024180303492b62f55cb3de3aab083c8b408c27e
H37kouya <h37kouya@gmail.com>
2019-11-22 16:25:09 +0900

    いっぱい

---
+++ Assets/PatternKinect/Rotator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotatespeed;
+    public GameObject CameraAxisObject;
+    public GameObject Rotateparameter;
+
+    void Start()
+    {
+        StartCoroutine("OnRotator");
+    }
+
+    IEnumerator OnRotator()
+    {
+        while (true)
+        {
+            if (DataCenter.IsAllDetected())
+            {
+                // float angle = (Input.GetAxis("Vertical") == 0 ?  Input.GetAxis("Horizontal") : Input.GetAxis("Vertical"))  * rotatespeed;
+                // カメラのオブジェクト
+                Vector3 Axis = CameraAxisObject.transform.position;
+                // 左手のオブジェクト
+                Vector3 rotateparrameter = Rotateparameter.transform.position;
+                Debug.Log(rotateparrameter.y);
+                if (rotateparrameter.y < 13)
+                {
+                    rotateparrameter.y *= -1;
+                }
+                float angle = rotateparrameter.y * rotatespeed;
+                //transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+                transform.RotateAround(Axis, Vector3.forward, angle);
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            yield return null;
+        }
+    }
+}
