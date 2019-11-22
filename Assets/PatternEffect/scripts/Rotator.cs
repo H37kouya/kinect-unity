84cca1f766b07711f07f2a79a1d964fe296f5947
toson <theawerqwer@gmail.com>
2019-11-22 09:49:02 +0900

    回転制御　試作

--- Assets/PatternEffect/scripts/Rotator.cs
+++ Assets/PatternEffect/scripts/Rotator.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour
 {
+    public float rotatespeed;
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        
+       
+        float angle = (Input.GetAxis("Vertical") == 0 ?  Input.GetAxis("Horizontal") : Input.GetAxis("Vertical"))  * rotatespeed;
+
+
+        //transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        transform.Rotate(0, 0, angle);
     }
 }
